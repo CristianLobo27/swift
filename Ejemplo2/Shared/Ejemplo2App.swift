54fb2c56ad516a9a0a0c5b772416fed87020668c
Cristian <cristianlobo2002@gmail.com>
2022-04-22 11:25:16 -0700

    he añadido

---
+++ Ejemplo2/Shared/Ejemplo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Ejemplo2App.swift
+//  Shared
+//
+//  Created by Alumno Swift on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Ejemplo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ejemplo5/Ejemplo5/Ejemplo5App.swift
@@ -0,0 +1,17 @@
+//
+//  Ejemplo5App.swift
+//  Ejemplo5
+//
+//  Created by Alumno Swift on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Ejemplo5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
